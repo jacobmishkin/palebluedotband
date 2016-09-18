7199e2dbb7377db7fb7ee59eb5c3633da35ab1b9
Jacob Mishkin <jacobmishkin@gmail.com>
2016-09-18 14:54:13 -0400

    fixed header wrapper

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <header class="navigation banner">
-    <div class="navigation-wrapper">
+  <div class="navigation-wrapper">
     <a href="<?php echo site_url(); ?>" class="logo">
       <img src="http://palebluedotband.dev/wp-content/uploads/2016/09/header-logo.png" alt="Pale Blue Dot Band">
     </a>
@@ -7,21 +7,22 @@
     <nav class="nav-primary" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation-menu show', 'menu_id' => 'js-navigation-menu']);
+      wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation-menu show', 'menu_id' => 'js-navigation-menu']);
       endif;
       ?>
     </nav>
+  </div>
   <section class="video-banner">
-  <?php
+    <?php
     $source = get_field('video_header');
     $image = get_field('header_sub_title');
     $poster = get_field('poster');
-  ?>
-  <video muted="true" poster="<?php echo $poster; ?>" autoplay="false" loop>
-    <source src="<?php echo $source; ?>" type="video/mp4">
-  </video>
-  <div class="sub-header">
-   <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-  </div>
-</section>
+    ?>
+    <video muted="true" poster="<?php echo $poster; ?>" autoplay="false" loop>
+      <source src="<?php echo $source; ?>" type="video/mp4">
+    </video>
+    <div class="sub-header">
+      <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+    </div>
+  </section>
 </header>
