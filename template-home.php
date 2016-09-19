5e60c549fddd272e4a945d8dda2efcd960c2a66b
Jacob Mishkin <jacobmishkin@gmail.com>
2016-09-19 11:11:07 -0400

    added tour module, changed mobile nav width to desktop view, changed music module to media module.

--- template-home.php
+++ template-home.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Template Name: Home Template
- */
+* Template Name: Home Template
+*/
 ?>
-
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/modules/module', 'music-player'); ?>
+	<?php get_template_part('templates/page', 'header'); ?>
+	<?php get_template_part('templates/modules/module', 'media'); ?>
+	<?php get_template_part('templates/modules/module', 'tour'); ?>
 <?php endwhile; ?>
---
+++ templates/modules/module-media.php
@@ -0,0 +1,18 @@
+<section class="modules module-media">
+	<div class="media-wrapper">
+		<div class="media-title">
+			<h2>music</h2>
+		</div>
+		<div class="media-player">
+			<?php echo do_shortcode('[wolf_jplayer_playlist id="1"]'); ?>
+		</div>
+		<div class="media-title">
+			<h2>video</h2>
+		</div>
+		<div class="media-player">
+			<span class="video-wrap">
+				<iframe src="https://www.youtube.com/embed/U08L3Vs95jQ" frameborder="0" allowfullscreen></iframe>
+			</span>
+		</div>
+	</div>
+</section>
