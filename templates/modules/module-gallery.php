cc91acc097ed342e6051200e4337b6b9159d4ef4
Jacob Mishkin <jacobmishkin@gmail.com>
2016-09-20 08:49:42 -0400

    removed side bar styles from pages.

--- templates/modules/module-gallery.php
+++ templates/modules/module-gallery.php
@@ -1,7 +1,7 @@
 <section class="module module-gallery">
 	<div class="container">
 		<div class="gallery-title">
-			<h2>Photos</h2>
+			<h2>Images</h2>
 		</div>
 		<div class="gallery-wrap">
 			<?php echo do_shortcode("[huge_it_gallery id='2']"); ?>
